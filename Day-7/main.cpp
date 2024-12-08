4e56e814ff8e8e0f0b18642c09231dddd704a805
szjaniak <szjaniak1@gmail.com>
2024-12-08 19:40:07 +0100

    Day-7: WIP
    part 1 done

--- Day-7/main.cpp
+++ Day-7/main.cpp
@@ -3,9 +3,36 @@
 #include <string>
 #include <vector>
 #include <sstream>
+        
+unsigned long long try_add(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx);        
+unsigned long long try_mul(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx);        
+
+unsigned long long try_add(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx) {
+    tmp_res += values[idx];
+    unsigned long long next_res;
+    if (tmp_res > test_value) return 0;
+    if (++idx == values.size()) return tmp_res;
+    next_res = try_add(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    next_res = try_mul(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    return 0;
+}
+
+unsigned long long try_mul(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx) {
+    tmp_res *= values[idx];
+    unsigned long long next_res;
+    if (tmp_res > test_value) return 0;
+    if (++idx == values.size()) return tmp_res;
+    next_res = try_add(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    next_res = try_mul(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    return 0;
+}
 
 int main(void) {
-    int result = 0;
+    unsigned long long result = 0;
     std::ifstream input{"input.txt"};
     if (!input.is_open()) {
         std::cerr << "error - file cannot be open" << std::endl;
@@ -16,16 +43,30 @@ int main(void) {
     size_t delimiter = 0;
     while (std::getline(input, line)) {
         delimiter = line.find(':');
-        long long test_value = std::stoll(line.substr(0, delimiter));
+        unsigned long long test_value = std::stoull(line.substr(0, delimiter));
+        std::cout << test_value << std::endl;
 
         std::istringstream iss(line.substr(delimiter + 1));
-        std::vector<long long> row;
-        long long value;
+        std::vector<unsigned long long> row;
+        unsigned long long value;
 
         while (iss >> value) {
             row.push_back(value);
         }
-        std::cout << std::endl; 
+
+        unsigned long long temp_result = row[0];
+        temp_result = try_add(test_value, temp_result, row, 1);        
+        if (temp_result == test_value) {
+            result += test_value;
+            std::cout << "FOUND: " << test_value << std::endl;
+            continue;
+        }
+        temp_result = row[0];
+        temp_result = try_mul(test_value, temp_result, row, 1);        
+        if (temp_result == test_value) { 
+            result += test_value;
+            std::cout << "FOUND: " << test_value << std::endl;
+        }
     }
     
     std::cout << "part 1 result: " << result << std::endl;
