74e672052f819e4e438c684ef64d873344f28d3b
szjaniak <szjaniak1@gmail.com>
2024-12-08 20:46:08 +0100

    Day-7: Final
    part 2 done

--- Day-7/main.cpp
+++ Day-7/main.cpp
@@ -3,36 +3,63 @@
 #include <string>
 #include <vector>
 #include <sstream>
+#include <inttypes.h>
         
-unsigned long long try_add(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx);        
-unsigned long long try_mul(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx);        
+uint64_t try_add(uint64_t test_value, uint64_t tmp_res, std::vector<uint64_t>& values, size_t idx);        
+uint64_t try_mul(uint64_t test_value, uint64_t tmp_res, std::vector<uint64_t>& values, size_t idx);        
+uint64_t try_con(uint64_t test_value, uint64_t tmp_res, std::vector<uint64_t>& values, size_t idx);        
+uint64_t concatenate(uint64_t fvalue, uint64_t svalue);
 
-unsigned long long try_add(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx) {
-    tmp_res += values[idx];
-    unsigned long long next_res;
+uint64_t concatenate(uint64_t fvalue, uint64_t svalue) {
+    std::string fvalue_str = std::to_string(fvalue);
+    std::string svalue_str = std::to_string(svalue);
+    return std::stoull(fvalue_str + svalue_str);
+}
+
+uint64_t try_con(uint64_t test_value, uint64_t tmp_res, std::vector<uint64_t>& values, size_t idx) {
+    tmp_res = concatenate(tmp_res, values[idx]);
     if (tmp_res > test_value) return 0;
     if (++idx == values.size()) return tmp_res;
+    uint64_t next_res;
     next_res = try_add(test_value, tmp_res, values, idx);
     if (next_res == test_value) return test_value;
     next_res = try_mul(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    next_res = try_con(test_value, tmp_res, values, idx);
     if (next_res == test_value) return test_value;
     return 0;
 }
 
-unsigned long long try_mul(unsigned long long test_value, unsigned long long tmp_res, std::vector<unsigned long long>& values, size_t idx) {
-    tmp_res *= values[idx];
-    unsigned long long next_res;
+uint64_t try_add(uint64_t test_value, uint64_t tmp_res, std::vector<uint64_t>& values, size_t idx) {
+    tmp_res += values[idx];
+    uint64_t next_res;
     if (tmp_res > test_value) return 0;
     if (++idx == values.size()) return tmp_res;
     next_res = try_add(test_value, tmp_res, values, idx);
     if (next_res == test_value) return test_value;
     next_res = try_mul(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    next_res = try_con(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    return 0;
+}
+
+uint64_t try_mul(uint64_t test_value, uint64_t tmp_res, std::vector<uint64_t>& values, size_t idx) {
+    tmp_res *= values[idx];
+    uint64_t next_res;
+    if (tmp_res > test_value) return 0;
+    if (++idx == values.size()) return tmp_res;
+    next_res = try_add(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    next_res = try_mul(test_value, tmp_res, values, idx);
+    if (next_res == test_value) return test_value;
+    next_res = try_con(test_value, tmp_res, values, idx);
     if (next_res == test_value) return test_value;
     return 0;
 }
 
 int main(void) {
-    unsigned long long result = 0;
+    uint64_t result = 0;
     std::ifstream input{"input.txt"};
     if (!input.is_open()) {
         std::cerr << "error - file cannot be open" << std::endl;
@@ -43,32 +70,23 @@ int main(void) {
     size_t delimiter = 0;
     while (std::getline(input, line)) {
         delimiter = line.find(':');
-        unsigned long long test_value = std::stoull(line.substr(0, delimiter));
-        std::cout << test_value << std::endl;
+        uint64_t test_value = std::stoull(line.substr(0, delimiter));
 
         std::istringstream iss(line.substr(delimiter + 1));
-        std::vector<unsigned long long> row;
-        unsigned long long value;
+        std::vector<uint64_t> values;
+        uint64_t value;
 
         while (iss >> value) {
-            row.push_back(value);
+            values.push_back(value);
         }
 
-        unsigned long long temp_result = row[0];
-        temp_result = try_add(test_value, temp_result, row, 1);        
+        uint64_t temp_result = try_add(test_value, 0, values, 0);        
         if (temp_result == test_value) {
-            result += test_value;
-            std::cout << "FOUND: " << test_value << std::endl;
-            continue;
-        }
-        temp_result = row[0];
-        temp_result = try_mul(test_value, temp_result, row, 1);        
-        if (temp_result == test_value) { 
             result += test_value;
             std::cout << "FOUND: " << test_value << std::endl;
         }
     }
     
-    std::cout << "part 1 result: " << result << std::endl;
+    std::cout << "result: " << result << std::endl;
     return 0;
 }
