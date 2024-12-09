a85db8ccf8deb20a737454b24f093532d04fc068
szjaniak1 <szjaniak1@gmail.com>
2024-12-09 22:33:32 +0100

    Day-8: WIP
    creating a map of antennas done

--- Day-8/main.cpp
+++ Day-8/main.cpp
@@ -1,12 +1,38 @@
 #include <fstream>
 #include <iostream>
+#include <map>
+#include <vector>
+#include <string>
 
 int main(void) {
-    std::fstream input{"input.txt"};
+    std::fstream input{"test_input.txt"};
     if (!input.is_open()) {
         std::cerr << "error - file cannot be open" << std::endl;
         return 1;
     }
+
+    std::string line;
+    std::map<char, std::vector<std::pair<size_t, size_t>>> antennas;
+    size_t row = 0;
+    while (input >> line) {
+        for (size_t col = 0; col < line.size(); col++) {
+            if (line[col] == '.') continue;
+            if (antennas.find(line[col]) != antennas.end()) {
+                antennas.find(line[col])->second.push_back({col, row});
+                continue;
+            }
+            antennas.insert({line[col], {{col, row}}});
+        }
+    }
+
+    for (auto& a : antennas) {
+        std::cout << a.first << " : ";
+        for (auto p : a.second) {
+            std::cout << "{" << p.first << ";" << p.second << "},";
+        }
+        std::cout << std::endl;
+    }
+
     int result = 0;
 
     std::cout << "part 1 result: " << std::endl;
