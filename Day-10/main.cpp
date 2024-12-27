898d805c555d9440cf19b7a45fb8574436a87465
szjaniak <szjaniak1@gmail.com>
2024-12-27 22:03:57 +0100

    Day-10 : WIP
    part 1 done

--- Day-10/main.cpp
+++ Day-10/main.cpp
@@ -8,21 +8,32 @@ using std::cout;
 using std::endl;
 using std::cerr;
 
-static uint find_all_paths(std::vector<std::vector<uint>>& topo_map, size_t row, size_t col) {
+static uint go_next(std::vector<std::vector<uint>>& topo_map, size_t row, size_t col) {
     uint result = 0;
-
-    
+    int val = (int)topo_map[row][col];
+    if (val == 9) {
+        topo_map[row][col] = 10;
+        return 1;
+    }
+    if (col < topo_map[0].size() - 1 && int(topo_map[row][col + 1]) - val == 1) result += go_next(topo_map, row, col + 1);
+    if (col > 0 && int(topo_map[row][col - 1]) - val == 1) result += go_next(topo_map, row, col - 1);
+    if (row < topo_map.size() - 1 && int(topo_map[row + 1][col]) - val == 1) result += go_next(topo_map, row + 1, col);
+    if (row > 0 && int(topo_map[row - 1][col]) - val == 1) result += go_next(topo_map, row - 1, col);
 
     return result;
 }
 
+static uint go_next_cp(std::vector<std::vector<uint>> topo_map, size_t row, size_t col) {
+    return go_next(topo_map, row, col);
+}
+
 int main(void) {
-    std::fstream input{"test_input.txt"};
+    std::fstream input{"input.txt"};
     if (!input.is_open()) {
         cerr << "error - cannot open the file!" << endl;
     }
 
-    constexpr size_t width = 8, height = 8;
+    constexpr size_t width = 54, height = 54;
     std::vector<std::vector<uint>> topographic_map;
     topographic_map.reserve(height);
 
@@ -41,8 +52,7 @@ int main(void) {
     for (size_t row = 0; row < height; row++) {
         for (size_t col = 0; col < width; col++) {
             if (topographic_map[row][col] != 0) continue;
-
-            result += find_all_paths(topographic_map, row, col);
+            result += go_next_cp(topographic_map, row, col);
         }
     }
 
@@ -52,6 +62,8 @@ int main(void) {
     //     }
     //     cout << endl;
     // }
+
+    cout << "part 1 result : " << result << endl;
 
     return 0;
 }
